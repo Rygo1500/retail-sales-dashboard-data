df78f731dae664543f470c788f7717eeec93e9c1
Rygo <134825016+Rygo1500@users.noreply.github.com>
2026-05-12 18:33:33 -0400

    Update tranformation.py
    Added comments

--- src/tranformation.py
+++ src/tranformation.py
@@ -4,37 +4,21 @@ from config import output_folder
 
 def consolidar_ventas(df_tienda, df_online, df_catalogo):
 
-    df_tienda.columns = [
-        'fecha_venta', 'hora', 'id_transaccion', 'sucursal',
-        'nombre_producto', 'cantidad', 'precio_unitario',
-        'vendedor', 'metodo_pago'
-    ]
+    #Estandarizar columnas
+    
+    df_tienda.columns = ['fecha_venta', 'hora', 'id_transaccion', 'sucursal', 'nombre_producto', 'cantidad', 'precio_unitario', 'vendedor', 'metodo_pago']
+    df_online.columns = ['fecha_venta', 'id_transaccion', 'nombre_producto', 'cantidad', 'precio_unitario', 'customer_email', 'city', 'status', 'shipping_cost']
 
-    df_online.columns = [
-        'fecha_venta', 'id_transaccion', 'nombre_producto',
-        'cantidad', 'precio_unitario', 'customer_email',
-        'city', 'status', 'shipping_cost'
-    ]
-
+    #Añadir canal
     df_tienda['canal'] = 'Tienda'
     df_online['canal'] = 'Online'
 
-    df_tienda = pd.merge(
-        df_tienda, df_catalogo,
-        left_on="nombre_producto", right_on="producto",
-        how="left"
-    )
+    #Añadir info de producto
+    df_tienda = pd.merge(df_tienda, df_catalogo, left_on="nombre_producto", right_on="producto", how="left")
+    df_online = pd.merge(df_online, df_catalogo, left_on="nombre_producto", right_on="producto", how="left")
 
-    df_online = pd.merge(
-        df_online, df_catalogo,
-        left_on="nombre_producto", right_on="producto",
-        how="left"
-    )
-
-    df_consolidado = pd.concat(
-        [df_tienda, df_online],
-        ignore_index=True
-    )
+    #Consolidar df
+    df_consolidado = pd.concat([df_tienda, df_online], axis = 0, ignore_index = True)
 
     return df_consolidado
 
@@ -46,26 +30,33 @@ def calcular_metricas(df):
     df['margen_bruto'] = df['total_venta'] - df['costo_total']
     df['margen_porcentual'] = (df['margen_bruto'] / df['total_venta']) * 100
 
+    #Calcular dia de la semana en español usando mapeo
+    
     dias = {
-        'Monday': 'Lunes',
-        'Tuesday': 'Martes',
-        'Wednesday': 'Miércoles',
-        'Thursday': 'Jueves',
-        'Friday': 'Viernes',
-        'Saturday': 'Sábado',
-        'Sunday': 'Domingo'
+    'Monday': 'Lunes',
+    'Tuesday': 'Martes',
+    'Wednesday': 'Miércoles',
+    'Thursday': 'Jueves',
+    'Friday': 'Viernes',
+    'Saturday': 'Sábado',
+    'Sunday': 'Domingo'
     }
 
     df['dia_semana'] = df['fecha_venta'].dt.day_name().map(dias)
 
-    df['semana_del_mes'] = df['fecha_venta'].apply(
-        lambda x: (x.day + x.replace(day=1).weekday()) // 7 + 1
-    )
+    #Calcular numero semana del mes
 
-    df['hora_num'] = pd.to_datetime(df['hora'], errors='coerce').dt.hour
+    df['semana_del_mes'] = df['fecha_venta'].apply(lambda x: (x.day + x.replace(day=1).weekday()) // 7+1)
+
+    #Calcular hora del dia
+
+    df['hora_num'] = pd.to_datetime(
+        df['hora'],
+        errors='coerce'
+    ).dt.hour
 
     df['hora_del_dia'] = df['hora_num'].apply(
-        lambda h: 'mañana' if pd.notna(h) and 6 <= h < 12
+        lambda h: 'mañana' if pd.notna(h) and 6 <= h <12
         else 'tarde' if pd.notna(h) and 12 <= h < 18
         else 'noche' if pd.notna(h)
         else None
@@ -73,6 +64,7 @@ def calcular_metricas(df):
 
     df.drop(columns='hora_num', inplace=True)
 
+    #Guardar df
     ruta = os.path.join(output_folder, "ventas_consolidadas.csv")
     df.to_csv(ruta, index=False)
 
