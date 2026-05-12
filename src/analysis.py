ad1a1ad1bd5f2edd7b80b622edbf08d445db7c1c
Rygo <134825016+Rygo1500@users.noreply.github.com>
2026-05-12 18:34:49 -0400

    Update analysis.py
    Added comments

--- src/analysis.py
+++ src/analysis.py
@@ -1,14 +1,17 @@
 import os
 from config import output_folder
 
-def analizar_devoluciones(df_ventas, df_devoluciones):
+def analizar_devoluciones(df_ventas,df_devoluciones):
 
+    #Normaliszar campos
     df_devoluciones['motivo'] = df_devoluciones['motivo'].str.capitalize()
     df_devoluciones['reembolsado'] = df_devoluciones['reembolsado'].str.capitalize()
 
+    #Guardar df
     ruta = os.path.join(output_folder, "devoluciones.csv")
     df_devoluciones.to_csv(ruta, index=False)
 
+    #Merge ventas y devoluciones
     df_analizado = df_ventas.merge(
         df_devoluciones,
         left_on='id_transaccion',
@@ -16,15 +19,12 @@ def analizar_devoluciones(df_ventas, df_devoluciones):
         how='left'
     )
 
+    #Verificar si la transaccion fue devuelta
     df_analizado['fue_devuelto'] = df_analizado['ticket_referencia'].notna()
-    df_analizado.drop(columns='ticket_referencia', inplace=True)
+    df_analizado = df_analizado.drop(columns='ticket_referencia')
 
-    df_analizado['tasa_devolucion_producto'] = (
-        df_analizado.groupby('nombre_producto')['fue_devuelto'].transform('mean')
-    )
-
-    df_analizado['tasa_devolucion_canal'] = (
-        df_analizado.groupby('canal')['fue_devuelto'].transform('mean')
-    )
+    #Calcular tasas de devolucion
+    df_analizado['tasa_devolucion_producto'] = df_analizado.groupby('nombre_producto')['fue_devuelto'].transform('mean')
+    df_analizado['tasa_devolucion_canal'] = df_analizado.groupby('canal')['fue_devuelto'].transform('mean')
 
     return df_analizado
