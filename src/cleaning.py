29c1e36221d5e6bcc909fa816c25e47a7b2d3aa8
Rygo <134825016+Rygo1500@users.noreply.github.com>
2026-05-12 18:34:18 -0400

    Update cleaning.py
    Added comments

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,35 +1,35 @@
 import pandas as pd
 
 def limpiar_ventas_tienda(df):
+    #Eliminar duplicados
+    df = df.drop_duplicates()
+    df = df.reset_index(drop=True)
 
-    df = df.drop_duplicates().reset_index(drop=True)
-
+    #Unificar fecha
     df['fecha'] = pd.to_datetime(df['fecha'], errors='coerce')
 
-    df['precio_unitario'] = pd.to_numeric(
-        df['precio_unitario']
-        .astype(str)
-        .str.strip()
-        .str.replace("$", "", regex=False),
-        errors='coerce'
-    )
+    #Limpiar precio
+    df['precio_unitario'] = pd.to_numeric(df['precio_unitario'].astype(str).str.strip().str.replace("$", "", regex=False), errors ='coerce')
 
+    #Normalizar metodo de pago
     df['metodo_pago'] = df['metodo_pago'].str.capitalize()
 
     return df
 
-
 def limpiar_ventas_online(df):
 
-    df = df.drop_duplicates().reset_index(drop=True)
+    #Eliminar duplicados
+    df = df.drop_duplicates()
+    df = df.reset_index(drop=True)
 
+    #Unificar fecha
     df['order_date'] = pd.to_datetime(df['order_date'], errors='coerce')
 
-    df.loc[
-        ~df['customer_email'].str.contains('@', na=False),
-        'customer_email'
-    ] = None
+    #Anular valores sin @
+    df_invalidos = df[~df['customer_email'].str.contains('@', na=False)]
+    df.loc[~df['customer_email'].str.contains('@', na=False), 'customer_email'] = None
 
+    #Normalizar status
     df['status'] = df['status'].str.capitalize()
 
     return df
